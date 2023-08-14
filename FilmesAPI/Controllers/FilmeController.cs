13aab086aa9289f0fff603d42f2e4b19596c1adc
Alex Santos <alxmath@gmail.com>
2023-08-14 17:49:29 -0300

    Add project files.

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,35 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> _filmes = new();
+    private static int _id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    {
+        filme.Id = _id++;
+        _filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperarFilmePorId), new { filme.Id }, filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+    {
+        return _filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id)
+    {
+        var filme = _filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme is null) { return NotFound(); }
+        return Ok(filme);
+    }
+}
